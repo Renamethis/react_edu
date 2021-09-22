c476c44ae8972fb048551e15c8025b2b13b580a6
Renamethis <ops282@mail.ru>
2021-09-22 23:45:37 +0300

    Init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
-import './App.css';
-
+import {Pulls} from './js/pulls.js';
+import {Issues} from './js/issues.js';
+import {Marketplace} from './js/marketplace.js';
+import {Explore} from './js/explore.js';
+import './css/App.css';
+import { BrowserRouter, Route, NavLink } from "react-router-dom";
+import bell from '/images/bell.png'
+import plus from '/images/plus.png'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <header className="App-header">
+          <div className="header-left">
+            <p className="icon">GETHUB</p>
+            <form>
+              <label>
+                  <input placeholder="Search or jump to..." className="search" type="text" name="name"/>
+              </label>
+            </form>
+            <li className="li_navbar">
+              <NavLink to="/pulls" className="title__text">Pull requests</NavLink>
+              <NavLink to="/issues" className="title__text">Issues</NavLink>
+              <NavLink to="/marketplace" className="title__text">Marketplace</NavLink>
+              <NavLink to="/explore" className="title__text">Explore</NavLink>
+            </li>
+          </div>
+        <div classname="header-right">
+          <NavLink to="/explore"><img className="bell" src={bell}></img></NavLink>
+          <NavLink to="/explore"><img className="plus" src={plus}></img></NavLink>
+        </div>
+        </header>
+      <Route path="/pulls"><Pulls/>  </Route>
+      <Route path="/issues"><Issues/>  </Route>
+      <Route path="/marketplace"><Marketplace/>  </Route>
+      <Route path="/explore"><Explore/>  </Route>
+      </BrowserRouter>
     </div>
   );
 }
-
 export default App;
---
+++ src/js/pulls.js
@@ -0,0 +1,7 @@
+export function Pulls() {
+  return (
+  <div className="Pulls">
+    Gnida
+  </div>
+  );
+}
