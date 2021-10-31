ee1919356072a8526109000cb89c709ae4823a92
ivan <ivan@localhost.localdomain>
2021-10-31 12:44:39 +0300

    Main page updated

--- src/js/Home.js
+++ src/js/Home.js
@@ -14,48 +14,160 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import ReactDOM from 'react-dom';
+import Grid from '@mui/material/Grid';
+import NewReleasesSharpIcon from '@mui/icons-material/NewReleasesSharp';
+import CircleSharpIcon from '@mui/icons-material/CircleSharp';
+import StarBorderIcon from '@mui/icons-material/StarBorder';
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height
+  };
+}
 export function Home() {
     const { currentUser, setCurrentUser } = useContext(AuthContext);
     if(!currentUser) {
         return <Redirect to="/login" />;
     }
     const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1
+        dots: true
       };
     return (
-      <BrowserRouter>
-            <div className="popular">
+        <BrowserRouter>
+        <Grid container alignItems="stretch">
+          <Grid item xs={6}>
+              <div className="popular">
+                <b className="big_text">Popular repositories</b>
                 <Slider className="slider" {...settings}>
-                <div>
-                    <div className="slider__container">
-                        <div className="slider__left-block">
-                            <div className="slider_item">
-                              <b className="slider-line head_text">protocolbuffers/protobuf</b>
-                            </div>
-                            <div className="slider_item">
-                                <span className="slider-line head_text">Description</span>
-                            </div>
-                            <div className="slider_item">
-                               <p className="default_text">Protocol Buffers - Google's data interchange format</p>
-                            </div>
-                        </div>
-                        <div className = "slider__right-block">
-                            <p className="slider-line head_text">Contributors</p>
-                            <p className="link_text">Renamethis</p>
-                            <p className="link_text">Hellothere</p>
-                            <p className="link_text">BuyThere</p>
-                        </div>
-                    </div>
-                </div>
-                <div>
-                    <h3>2</h3>
-                </div>
+                  <div>
+                      <div className="slider__container">
+                          <div className="slider__left-block">
+                              <div className="slider_item">
+                                <b className="slider-line head_text">protocolbuffers/protobuf</b>
+                              </div>
+                              <div className="slider_item">
+                                  <span className="slider-line head_text">Description</span>
+                              </div>
+                              <div className="slider_item">
+                                 <p className="default_text">Protocol Buffers (a.k.a., protobuf) are Google's language-neutral, platform-neutral, extensible mechanism for serializing structured data. You can find protobuf's documentation on the Google Developers site.</p>
+                              </div>
+                          </div>
+                          <div className = "slider__right-block">
+                              <p className="slider-line head_text">Contributors</p>
+                              <p className="link_text slider-line">Renamethis</p>
+                              <p className="link_text slider-line">deannagarcia</p>
+                              <p className="link_text slider-line">Roger Knapp</p>
+                              <p className = "small_text"> + 774 contributors </p>
+                          </div>
+                      </div>
+                  </div>
+                  <div>
+                  <div className="slider__container">
+                      <div className="slider__left-block">
+                          <div className="slider_item">
+                            <b className="slider-line head_text">opencv/opencv</b>
+                          </div>
+                          <div className="slider_item">
+                              <span className="slider-line head_text">Description</span>
+                          </div>
+                          <div className="slider_item">
+                             <p className="default_text">Open Source Computer Vision Library</p>
+                          </div>
+                      </div>
+                      <div className = "slider__right-block">
+                          <p className="slider-line head_text">Contributors</p>
+                          <p className="slider-line link_text">ozantonkal</p>
+                          <p className="slider-line link_text">Alexander Karsakov</p>
+                          <p className="slider-line link_text">Vitaly Tuzov</p>
+                          <p className = "small_text"> + 1,306 contributors </p>
+                      </div>
+                  </div>
+                  </div>
                 </Slider>
-            </div>
-      </BrowserRouter>
+              </div>
+              <div className="activity">
+              <b className="big_text">Activity</b>
+              <p className="head_text_activity"><img className="avatar" src="https://avatars.githubusercontent.com/u/29013070?s=20&v=4"/><b>Nickname1 </b>&nbsp;pushed to <b>Nickname1/test_repo</b></p>
+              <div className="activity_tab">
+                <p className="activity_text">1 commit to <p className="branch">master</p></p>
+                <p className="activity_text"><a href="#">12x4sa</a> Update</p>
+              </div>
+              <p className="head_text_activity"><img className="avatar" src="https://avatars.githubusercontent.com/u/29013070?s=20&v=4"/><b>Renamethis </b>&nbsp;pushed to <b>Nickname1/test_repo</b></p>
+              <div className="activity_tab">
+                <p className="activity_text">1 commit to <p className="branch">dev_branch_v1.01</p></p>
+                <p className="activity_text"><a href="#">ab2fha</a> New system module</p>
+              </div>
+              <p className="head_text_activity"><img className="avatar" src="https://avatars.githubusercontent.com/u/29013070?s=20&v=4"/><b>opencv </b>&nbsp;pushed to <b>Nickname1/test_repo</b></p>
+              <div className="activity_tab">
+                <p className="activity_text">1 commit to <p className="branch">opencv_4.3.0</p></p>
+                <p className="activity_text"><a href="#">t9s4f2</a> Updated cv::Size </p>
+              </div>
+              </div>
+              </Grid>
+              <Grid item xs>
+              <div className="right_panel">
+              <b className="big_text">Announce</b>
+                <div className="announce">
+                  <div className="announce_item">
+                    <NewReleasesSharpIcon /> <b className="head_text">Save the data!</b>
+                  </div>
+                  <div className="announce_item">
+                    <p className="small_text"> GitHub Universe is coming October 27 and 28. From product deep dives to interactive roundtables, you’ll gather the tips, tools, and connections to help you do the best work of your life. </p>
+                  </div>
+                  <button className="announce_item learn_button" type="submit">Learn More</button>
+                </div>
+                <div className="explore_repos">
+                  <b className="head_text"> Explore repositories </b>
+                  <div className="explores_item">
+                    <p className="head_text">murtazahassan/Face-Recognition</p>
+                    <p className="small_text">A Python project which can detect gender and age using OpenCV of the person (face) in a picture or through webcam. </p>
+                    <div className="explores_container">
+                      <div className="explores_line">
+                        <CircleSharpIcon className="circle" />
+                        <p className="explores_text">Python</p>
+                      </div>
+                      <div className="explores_line">
+                        <StarBorderIcon />
+                        <p className="explores_text">65</p>
+                      </div>
+                      </div>
+                  </div>
+                  <div className="line" />
+                  <div className="explores_item">
+                    <p className="head_text">smahesh29/Gender-and-Age-Detection</p>
+                      <p className="small_text">Multi-view Face Detection Using Deep Convolutional Neural Networks  </p>
+                    <div className="explores_container">
+                      <div className="explores_line">
+                        <CircleSharpIcon className="circle" />
+                        <p className="explores_text">Python</p>
+                      </div>
+                      <div className="explores_line">
+                        <StarBorderIcon />
+                        <p className="explores_text">137</p>
+                      </div>
+                      </div>
+                  </div>
+                  <div className="line" />
+                  <div className="explores_item">
+                    <p className="head_text">guoyilin/FaceDetection_CNN</p>
+                      <p className="small_text">Scalable, Portable and Distributed Gradient Boosting (GBDT, GBRT or GBM) Library, for Python, R, Java, Scala, C++ and more. Runs on single machine, Hadoop, Spark, Dask, Flink and DataFlow  </p>
+                    <div className="explores_container">
+                      <div className="explores_line">
+                        <CircleSharpIcon className="circle_pink" />
+                        <p className="explores_text">C++</p>
+                      </div>
+                      <div className="explores_line">
+                        <StarBorderIcon />
+                        <p className="explores_text">137</p>
+                      </div>
+                      </div>
+                  </div>
+                  <div className="line" />
+                </div>
+              </div>
+            </Grid>
+          </Grid>
+        </BrowserRouter>
     );
 }
--- src/js/Layout.js
+++ src/js/Layout.js
@@ -1,69 +1,92 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
-import bell from "../images/bell.png";
-import plus from "../images/plus.png";
 import repo from "../images/repo.png";
-import Dropdown from 'react-dropdown';
-const plus_options = [
-  'New repository', 'Import repository', 'New gist', 'New organization'
-];
+import IconButton from '@mui/material/IconButton';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import PlusIcon from '@material-ui/icons/ControlPoint';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
 export const Layout = ({children}) => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
   <div className="Main">
-    <header className="App-header">
-      <div className="header-left">
-        <NavLink style={{textDecoration: 'none', color: 'white'}} to="/">
-          <p className="icon">GETHUB</p>
-        </NavLink>
-        <form>
-          <label>
-            <input placeholder="Search or jump to..." className="search" type="text" name="name"/>
-          </label>
-        </form>
-        <li className="li_navbar">
-          <NavLink to="/pulls" className="title__text">Pull requests</NavLink>
-          <NavLink to="/issues" className="title__text">Issues</NavLink>
-          <NavLink to="/marketplace" className="title__text">Marketplace</NavLink>
-          <NavLink to="/explore" className="title__text">Explore</NavLink>
-        </li>
+      <header className="App-header">
+        <div className="header-left">
+          <NavLink style={{textDecoration: 'none', color: 'white'}} to="/">
+            <p className="icon">GETHUB</p>
+          </NavLink>
+          <form>
+            <label>
+              <input placeholder="Search or jump to..." className="search" type="text" name="name"/>
+            </label>
+          </form>
+          <li className="li_navbar">
+            <NavLink to="/pulls" className="title__text">Pull requests</NavLink>
+            <NavLink to="/issues" className="title__text">Issues</NavLink>
+            <NavLink to="/marketplace" className="title__text">Marketplace</NavLink>
+            <NavLink to="/explore" className="title__text">Explore</NavLink>
+          </li>
+        </div>
+        <div className="header-right">
+          <NavLink to="/explore">
+            <NotificationsNoneIcon className="bell"/>
+          </NavLink>
+          <IconButton component="span" onClick={handleClick}>
+            <PlusIcon className="plus" />
+          </IconButton>
+            <Menu
+          id="demo-positioned-menu"
+          aria-labelledby="demo-positioned-button"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'left',
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'left',
+          }}
+        >
+          <MenuItem onClick={handleClose}>New repository</MenuItem>
+          <MenuItem onClick={handleClose}>Import repository</MenuItem>
+          <MenuItem onClick={handleClose}>New gist</MenuItem>
+          <MenuItem onClick={handleClose}>New organization</MenuItem>
+        </Menu>
+        </div>
+      </header>
+      <div className="sidebar">
+        <div className="nickname">
+          <img className="avatar" src="https://avatars.githubusercontent.com/u/29013070?s=20&v=4"/>
+          <p>Nickname</p>
+        </div>
+        <div className="line"></div>
+        <div className="reps">
+          <b>Repositories</b>
+          <button className="btn" type="submit">New</button>
+        </div>
+        <input placeholder="Find a repository..." className="frep" type="text" name="name"/>
+        <div className="links">
+          <a href="#"><img className="repo_img" src={repo}/>Nickname/Repo1</a>
+          <a href="#"><img className="repo_img" src={repo}/>Nickname/Repo2</a>
+          <a href="#"><img className="repo_img" src={repo}/>Nickname/Repo3</a>
+        </div>
+        <div className="line"></div>
+        <b className="head_text">Recent Activity</b>
+        <p className="small_text">When you take actions across GitHub, we’ll provide links to that activity here.</p>
       </div>
-      <div className="header-right">
-        <NavLink to="/explore"><img className="bell" src={bell}></img></NavLink>
-        <Dropdown>
-          <Dropdown.Toggle variant="success" id="dropdown-basic">
-          </Dropdown.Toggle>
-
-          <Dropdown.Menu>
-            <Dropdown.Item href="/explore">Action</Dropdown.Item>
-            <Dropdown.Item href="/explore">Another action</Dropdown.Item>
-            <Dropdown.Item href="/explore">Something else</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
+      <div style={{marginLeft: '252px'}}>
+        {children}
       </div>
-    </header>
-    <div className="sidebar">
-      <div className="nickname">
-        <img className="avatar" src="https://avatars.githubusercontent.com/u/29013070?s=20&v=4"/>
-        <p>Nickname</p>
-      </div>
-      <div className="line"></div>
-      <div className="reps">
-        <b>Repositories</b>
-        <button className="btn" type="submit">New</button>
-      </div>
-      <input placeholder="Find a repository..." className="frep" type="text" name="name"/>
-      <div className="links">
-        <a href="#"><img className="repo_img" src={repo}/>Nickname/Repo1</a>
-        <a href="#"><img className="repo_img" src={repo}/>Nickname/Repo2</a>
-        <a href="#"><img className="repo_img" src={repo}/>Nickname/Repo3</a>
-      </div>
-      <div className="line"></div>
-      <b className="head_text">Recent Activity</b>
-      <p className="small_text">When you take actions across GitHub, we’ll provide links to that activity here.</p>
-    </div>
-    <div style={{marginLeft: '252px'}}>
-      {children}
-    </div>
   </div>
   )
 }
